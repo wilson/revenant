4cdd3301c1915e2cff3551debc94dd84aff8eead
Wilson Bilkovich <wilson@supremetyrant.com>
2010-04-02 16:14:50 -0400

    No need to require revenant/task or revenant/daemon anymore

--- spec/lib/revenant/task_spec.rb
+++ spec/lib/revenant/task_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'revenant/task'
 
 describe Revenant::Task do
   before do
--- spec/process/daemon_signal_spec.rb
+++ spec/process/daemon_signal_spec.rb
@@ -1,5 +1,4 @@
 require 'spec_helper'
-require 'revenant/daemon'
 
 describe Revenant::Task do
   before do
