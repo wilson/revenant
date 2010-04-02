a655188f08926c629ac3205cdca943085a9c815a
Wilson Bilkovich <wilson@supremetyrant.com>
2010-04-02 12:19:43 -0400

    Some placeholder specs for signal handling

---
+++ spec/process/daemon_signal_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+require 'revenant/daemon'
+
+describe Revenant::Task do
+  before do
+    @task = Revenant::Task.new("daemonsignal")
+    @task.stubs(:exit)
+  end
+
+  it "shuts down on SIGTERM" do
+    pending
+  end
+
+  it "restarts on SIGUSR2" do
+    pending
+  end
+end
+
---
+++ spec/process/signal_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'revenant/task'
+
+describe Revenant::Task do
+  before do
+    @task = Revenant::Task.new("signalspec")
+    @task.stubs(:exit)
+  end
+
+  it "shuts down on SIGINT when in the foreground" do
+    pending
+    @task.should_not be_shutdown_pending
+    @task.should_not be_restart_pending
+  end
+end
+
